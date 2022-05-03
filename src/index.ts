4e1722464899e8c8ca6f0ab9dab06f650abdc54e
RDSV15 <janseyrds@hotmail.com>
2022-05-03 15:10:28 -0300

    funcao que pede nome dos alunos

--- src/index.ts
+++ src/index.ts
@@ -33,11 +33,48 @@ app.use(express.json());
  * Server Activation
  */
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
 	console.log(`Listening on port ${PORT}`);
 
 
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
 	
+	const { Sequelize } = require('sequelize');
+
+	const sequelize = new Sequelize({
+		dialect: 'sqlite',
+		storage: 'path/to/database.sqlite'
+	});
+
+	const readline = require('readline');
+		const leitor = readline.createInterface({
+    	input: process.stdin,
+    	output: process.stdout
+	});
+
+	const DAO: any[] = [];
+
+	leitor.question('Digite a quantidade de alunos: ', async (quantidade: number) => {
+    	for(let i = 0; i < quantidade; i++) {
+        	
+       	 	await new Promise((resolve) => { 
+            	leitor.question(`Digite o seu nome ${i}: `, (nome: any) => {
+                	resolve(DAO.push(nome)); 
+					
+            	});
+        	});
+		}
+		leitor.close();
+    	console.log(DAO);
+		
+	});
+	
+	try {
+		await sequelize.authenticate();
+		console.log('Connection has been established successfully.');
+	  } catch (error) {
+		console.error('Unable to connect to the database:', error);
+	  }
+
 });
